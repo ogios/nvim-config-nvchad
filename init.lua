83b0dca445d114cf3d60f805532cc3374d2ae4be
ogios <2134692955@qq.com>
2023-08-08 14:09:18 +0800

    add neovide options

--- init.lua
+++ init.lua
@@ -3,3 +3,7 @@ require("custom.ftplugin.python")
 require("custom.ftplugin.go")
 require("custom.keymap")
 require("custom.configs.go_conf")
+
+if vim.g.neovide then
+	require("custom.neovide")
+end
