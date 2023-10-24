97c88e50702834cc0108cfc198b2a6ceba2e2fab
ogios <2134692955@qq.com>
2023-10-24 09:07:38 +0800

    add telescope emoji

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,16 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    {
+      "xiyaowong/telescope-emoji.nvim",
+      keys = {
+        { "<A-.>", "<CMD>Telescope emoji<CR>", "telescope emoji select" },
+      },
+      config = function()
+        require("telescope").load_extension("emoji")
+      end,
+    },
+  },
   opts = function(_, opts)
     if opts.defaults.vimgrep_arguments ~= nil then
       vim.list_extend(opts.defaults.vimgrep_arguments, {
