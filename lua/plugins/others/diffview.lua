c189051ff152a4867f1ed4c28915c4bb959c06b8
ogios <2134692955@qq.com>
2024-03-22 13:12:39 +0800

    diffview add module

--- lua/plugins/others/diffview.lua
+++ lua/plugins/others/diffview.lua
@@ -3,8 +3,22 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFileHistory" },
   config = function()
-    require("diffview").setup({})
+    -- local info = require("lazyvim.util").info
+    require("diffview").setup({
+      -- hooks = {
+      --   view_opened = function(view)
+      --     info(vim.inspect(view))
+      --   end,
+      --   diff_buf_read = function(bufnr, ctx)
+      --     info("view diff_buf_read: " .. tostring(bufnr))
+      --   end,
+      --   diff_buf_win_enter = function(bufnr, winid, ctx)
+      --     info("diff_buf_win_enter：" .. tostring(bufnr) .. " " .. tostring(winid))
+      --   end,
+      -- },
+    })
   end,
+  module = "diffview",
   keys = {
     { "<leader>dvo", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
     { "<leader>dvc", "<cmd>DiffviewClose<cr>", desc = "Diffview" },
