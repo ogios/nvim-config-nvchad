ff120be5fd064f6d3577660c789982233e2cccc6
ogios <2134692955@qq.com>
2024-05-22 12:58:33 +0800

    remove mini comment

--- lua/plugins/code/editor.lua
+++ lua/plugins/code/editor.lua
@@ -86,25 +86,25 @@ return {
     end,
   },
 
-  {
-    "echasnovski/mini.comment",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      if opts.hooks == nil then
-        opts.hooks = {}
-      end
-      if opts.mappings == nil then
-        opts.mappings = {}
-      end
+  -- {
+  --   "echasnovski/mini.comment",
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     if opts.hooks == nil then
+  --       opts.hooks = {}
+  --     end
+  --     if opts.mappings == nil then
+  --       opts.mappings = {}
+  --     end
 
-      opts.hooks.pre = function()
-        require("ts_context_commentstring.internal").update_commentstring()
-      end
-      -- opts.mappings.comment = "<c-/>"
-      -- opts.mappings.comment_line = "<c-/><c-/>"
-      -- opts.mappings.textobject = "<leader>//"
-    end,
-  },
+  --     opts.hooks.pre = function()
+  --       require("ts_context_commentstring.internal").update_commentstring()
+  --     end
+  --     -- opts.mappings.comment = "<c-/>"
+  --     -- opts.mappings.comment_line = "<c-/><c-/>"
+  --     -- opts.mappings.textobject = "<leader>//"
+  --   end,
+  -- },
 
   {
     "hrsh7th/nvim-cmp",
