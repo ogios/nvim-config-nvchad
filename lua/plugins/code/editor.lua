3eb3434e28ed85a44e962c3ca7d1a1ff91ba76ba
ogios <ogios@foxmail.com>
2024-11-28 16:07:36 +0800

    update select behavier

--- lua/plugins/code/editor.lua
+++ lua/plugins/code/editor.lua
@@ -113,8 +113,8 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
       })
     end,
   },
